2548761861b824a0558482bf5d85090a994ea1da
moforemmanuel <moforemmanuel722@gmail.com>
2020-12-17 17:17:12 +0100

    Initial commit

---
+++ app/src/main/java/com/codeboy/viewpager2transformers/MainActivity.java
@@ -0,0 +1,19 @@
+package com.codeboy.viewpager2transformers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
