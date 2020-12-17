a4e681a4648b88553c351216277ed51cff260721
moforemmanuel <moforemmanuel722@gmail.com>
2020-12-17 17:22:56 +0100

    pager2 implementation

---
+++ app/src/main/java/com/codeboy/viewpager2transformers/Pager2RecyclerAdapter.java
@@ -0,0 +1,26 @@
+package com.codeboy.viewpager2transformers;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class Pager2RecyclerAdapter extends RecyclerView.Adapter<Pager2ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public Pager2ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Pager2ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/codeboy/viewpager2transformers/Pager2ViewHolder.java
@@ -0,0 +1,12 @@
+package com.codeboy.viewpager2transformers;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Pager2ViewHolder extends RecyclerView.ViewHolder {
+    public Pager2ViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
