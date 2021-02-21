6ce816fb75cadc4e447bbacfbee030c0fe4d67dd
Codeboy722 <codeboy722@gmail.com>
2021-02-21 17:28:51 +0100

    update

--- Pager2_Transformers/src/main/java/com/codeboy/pager2_transformers/Pager2_VerticalFlipTransformer.java
+++ Pager2_Transformers/src/main/java/com/codeboy/pager2_transformers/Pager2_VerticalFlipTransformer.java
@@ -8,12 +8,13 @@ import androidx.viewpager2.widget.ViewPager2;
 public class Pager2_VerticalFlipTransformer implements ViewPager2.PageTransformer {
     @Override
     public void transformPage(@NonNull View page, float position) {
-        page.setTranslationX(-position * page.getWidth());
-        page.setCameraDistance(12000);
+        page.setTranslationX(-position*page.getWidth());
+        page.setCameraDistance(20000);
 
-        if (position < 0.5 && position > -0.5) {
+        if (position < 0.5 && position > -0.5){
             page.setVisibility(View.VISIBLE);
-        } else {
+        }
+        else {
             page.setVisibility(View.INVISIBLE);
         }
 
@@ -21,18 +22,17 @@ public class Pager2_VerticalFlipTransformer implements ViewPager2.PageTransforme
             // This page is way off-screen to the left.
             page.setAlpha(0);
         }
-        else if (position <= 0) {    // [-1,0]
+        else if (position <= 0 ){    // [-1,0]
             page.setAlpha(1);
-            page.setRotationY(180 *(1-Math.abs(position)+1));
+            page.setRotationX(180*(1-Math.abs(position)+1));
         }
-        else if (position <= 1) {    // (0,1]
+        else if (position <= 1){    // (0,1]
             page.setAlpha(1);
-            page.setRotationY(-180 *(1-Math.abs(position)+1));
+            page.setRotationX(-180*(1-Math.abs(position)+1));
         }
         else {    // (1,+Infinity]
             // This page is way off-screen to the right.
             page.setAlpha(0);
         }
-
     }
 }
